6bb75de7ee1a2044e629e93058579a3905b79e9c
goodleaf96 <ky1234tho@gmail.com>
2025-02-11 11:27:19 +0900

    정보 검색 기능 엔드포인트 추가

--- src/main/java/com/example/idus_exam/user/UserController.java
+++ src/main/java/com/example/idus_exam/user/UserController.java
@@ -3,7 +3,9 @@ package com.example.idus_exam.user;
 import com.example.idus_exam.order.Order;
 import com.example.idus_exam.order.OrderRepository;
 import com.example.idus_exam.user.model.User;
+import com.example.idus_exam.user.model.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,10 @@ public class UserController {
     @GetMapping("/orders")
     public List<Order> getUserOrders(@AuthenticationPrincipal User user) {
         return orderRepository.findByUser(user);
+    }
+
+    @GetMapping("/myinfo")
+    public ResponseEntity<UserDto.UserResponse> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(UserDto.UserResponse.from(user));
     }
 }
