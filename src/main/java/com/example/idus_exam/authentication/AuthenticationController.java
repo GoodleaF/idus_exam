1c2c30f7a7aeb6164881caa2ec9c3d1acb497039
goodleaf96 <ky1234tho@gmail.com>
2025-02-11 11:21:06 +0900

    로그아웃 기능 추가

--- src/main/java/com/example/idus_exam/authentication/AuthenticationController.java
+++ src/main/java/com/example/idus_exam/authentication/AuthenticationController.java
@@ -2,12 +2,15 @@ package com.example.idus_exam.authentication;
 
 import com.example.idus_exam.user.UserService;
 import com.example.idus_exam.user.model.UserDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,13 +21,19 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserDto.LoginRequest loginRequest) {
-        // Create an authentication token using email and password.
         UsernamePasswordAuthenticationToken token =
                 new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
-        // Authenticate the token.
         Authentication authentication = authenticationManager.authenticate(token);
-        // Set the authenticated user into the SecurityContext.
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return ResponseEntity.ok("Login successful");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok("Logout successful");
     }
 }
